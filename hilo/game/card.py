41091a724117f00a08779b25e49fb7f08db2709d
Seaver-William <31837085+Seaver-William@users.noreply.github.com>
2021-10-09 20:22:03 -0700

    updates please test

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -1,20 +1,20 @@
 import random
 
+
 class Card():
 
-    def __init__(self):
-        # self.display = random.randint(1,13)
-        # #self.score = 300
-        # #self.guess_card = random.randint(1,13)
-        # self.displayed_card = random.randint(1,13)
-        # self.next_displayed_card = random.randint(1,13)
+    # def __init__(self):
 
-        #self.display = 0
-        #self.guess_card = 0
-    
+    # self.display = random.randint(1,13)
+    # #self.score = 300
+    # #self.guess_card = random.randint(1,13)
+    # self.displayed_card = random.randint(1,13)
+    # self.next_displayed_card = random.randint(1,13)
+
+    #self.display = 0
+    #self.guess_card = 0
 
     def guess_high(self, card1_num, card2_num):
-
         return card1_num < card2_num
     # def next_guess_high(self):
     #     return self.next_displayed_card > self.displayed_card
@@ -25,9 +25,9 @@ class Card():
     #     return self.next_displayed_card < self.displayed_card
 
     def get_random_num(self):
-        new_ran_num = random.randint(1,13)
+        new_ran_num = random.randint(1, 13)
         return new_ran_num
-        
+
     """def add_points(self):
         return  100
     def sub_points(self):
